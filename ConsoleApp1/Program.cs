4067b32dea931d965ef50e5b4c46506e7673193f
githubmmtiprog <prog_common2@makoto.com.ph>
2025-05-02 14:27:37 +0800

    testing branch

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Port=3306;Database=mydatabase;User=mark;Password=mark;";
+        MySqlConnection connection = new(connectionString);
+
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connected to MySQL!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ trial.cs
@@ -0,0 +1,25 @@
+using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Port=3306;Database=mydatabase;User=mark;Password=mark;";
+        MySqlConnection connection = new MySqlConnection(connectionString);
+
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connected to MySQL!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+}
